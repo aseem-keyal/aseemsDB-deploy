66ec2eeb00187a0bfe69dc9f57b7efbe3d06c59c
Aseem Keyal <aseem.keyal@gmail.com>
2020-12-16 05:50:27 +0000

    dockerized s3 backup, use variable for all host names in traefik, update grafana dashboard

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ~
-tar czf packet_archive.tar.gz packet_archive
-aws s3 cp packet_archive.tar.gz s3://aseemsdb-packet-archive
-tar czf recollconf.tar.gz recollconf
-aws s3 cp recollconf.tar.gz s3://aseemsdb-packet-archive
-rm packet_archive.tar.gz recollconf.tar.gz
--- staging.sh
+++ staging.sh
@@ -1,5 +1,6 @@
 export USERNAME=admin
 export EMAIL=aseem.keyal@gmail.com
-export DOMAIN=traefik.testing.aseemsdb.me
 export ASEEMSDB_DOMAIN=testing.aseemsdb.me
 export HASHED_PASSWORD=$(openssl passwd -apr1)
+export S3_PATH="s3://aseemsdb-packet-archive/"
+export CRON_SCHEDULE="0 12 1 * *"
