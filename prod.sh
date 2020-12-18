278c26db86af6ac67ecf33880fee35ef986fc58a
Aseem Keyal <aseem.keyal@gmail.com>
2020-12-18 13:54:12 +0000

    add caching for nginx static files, update grafana datasource

---
+++ prod.sh
@@ -0,0 +1,6 @@
+export USERNAME=admin
+export EMAIL=aseem.keyal@gmail.com
+export ASEEMSDB_DOMAIN=aseemsdb.me
+export HASHED_PASSWORD=$(openssl passwd -apr1)
+export S3_PATH="s3://aseemsdb-packet-archive/"
+export CRON_SCHEDULE="0 12 1 * *"
