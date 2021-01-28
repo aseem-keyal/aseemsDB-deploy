c131e8a39b92b0005644ddcb74b489f2ac9f4829
Aseem Keyal <aseem.keyal@gmail.com>
2021-01-28 12:51:27 -0800

    added testing.yml docker-compose file for testing env

--- staging.sh
+++ staging.sh
@@ -2,5 +2,3 @@ export USERNAME=admin
 export EMAIL=aseem.keyal@gmail.com
 export ASEEMSDB_DOMAIN=testing.aseemsdb.me
 export HASHED_PASSWORD=$(openssl passwd -apr1)
-export S3_PATH="s3://aseemsdb-packet-archive/"
-export CRON_SCHEDULE="0 12 1 * *"
